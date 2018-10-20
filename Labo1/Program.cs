0b5b7d6d95dd93ad87096c12fef7b760e7c71da6
Valentina <valentina.rosina@hotmail.com>
2018-10-20 15:21:30 +0200

    inserito char

--- Labo1/Program.cs
+++ Labo1/Program.cs
@@ -7,6 +7,24 @@ namespace Labo1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int x = Console.Read();
+            int a = 0;
+            int b = 1;
+            int [] c = new int [5];
+            for(int i=0; i<5; i++)
+            {
+                c[i] = b;
+                a = i;
+                b++;
+            }
+            Console.WriteLine("a={0}",a);
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine("c[{0}]={1}", i, c[i]);
+            }
+            char ch = Convert.ToChar(x);
+            Console.WriteLine("x={0}",ch);
+            Console.Read();
         }
     }
 }
