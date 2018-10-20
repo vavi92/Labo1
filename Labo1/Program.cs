803c016b64aafd2649f879dff074338300510b49
Valentina <valentina.rosina@hotmail.com>
2018-10-20 11:15:00 +0200

    Aggiungere i file di progetto.

---
+++ Labo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
